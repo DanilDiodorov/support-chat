732a192452dda1492206188b2a02d6036f8e4357
Диодоров Данил Леонидович <diodorov.dl@rcit.sakha.gov.ru>
2024-01-31 14:10:04 +0900

    added: notification sound

--- client/src/features/socket/hooks/use-socket.js
+++ client/src/features/socket/hooks/use-socket.js
@@ -5,6 +5,7 @@ import { selectCurrentUser } from 'entities/user'
 import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { io } from 'socket.io-client'
+import messageSound from './../assets/message.mp3'
 
 let flag = false
 
@@ -13,6 +14,7 @@ export const useSocket = () => {
     const [socket, setSocket] = useState(null)
     const dispatch = useDispatch()
     const queryClient = useQueryClient()
+    const [isTabActive, setIsTabActive] = useState(true)
 
     useEffect(() => {
         if (user && !flag) {
@@ -33,10 +35,30 @@ export const useSocket = () => {
     }, [user])
 
     useEffect(() => {
+        const handleVisibilityChange = () => {
+            console.log(!document.hidden)
+            setIsTabActive(!document.hidden)
+        }
+
+        document.addEventListener('visibilitychange', handleVisibilityChange)
+
+        return () => {
+            document.removeEventListener(
+                'visibilitychange',
+                handleVisibilityChange
+            )
+        }
+    }, [])
+
+    useEffect(() => {
         socket?.on('chat:add', (data) => {
             dispatch(addChat(data))
         })
         socket?.on('message:add', (data) => {
+            const audio = new Audio(messageSound)
+            try {
+                if (!isTabActive) audio.play()
+            } catch (error) {}
             dispatch(addMessage(data))
         })
         socket?.on('chat:transfer', () => {
@@ -46,5 +68,5 @@ export const useSocket = () => {
         return () => {
             socket?.removeAllListeners()
         }
-    }, [socket, dispatch, queryClient])
+    }, [socket, dispatch, queryClient, isTabActive])
 }
